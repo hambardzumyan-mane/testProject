669c497948b85dd133aa913c844c95c7dff585fd
Mane <mane@Mane.local>
2016-08-17 15:43:44 +0400

    Integrated Firebase framework
    - Added podfile for Firebase framework integration
    - Added functionality for Sign In / Sign Out through email and password
    - Added functionality to parse QR and BAR codes from camera device
    - Added Stock List, Room List, Item list and status showing pages

---
+++ QIASmartStockApp/Pods/Target Support Files/Pods-QIASmartStockApp/Pods-QIASmartStockApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_QIASmartStockAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_QIASmartStockAppVersionString[];
+
---
+++ QIASmartStockApp/QIASmartStockApp/Application/MainMenu/StockInfoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SockInfoViewController.swift
+//  QIASmartStockApp
+//
+//  Created by Mane Hambardzumyan on 8/15/16.
+//  Copyright © 2016 Instigate Mobile. All rights reserved.
+//
+
+import UIKit
+
+class StockInfoViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	/*
+	 // MARK: - Navigation
+
+	 // In a storyboard-based application, you will often want to do a little preparation before navigation
+	 override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+	 // Get the new view controller using segue.destinationViewController.
+	 // Pass the selected object to the new view controller.
+	 }
+	 */
+
+}
